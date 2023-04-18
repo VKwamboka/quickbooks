339d95967ea1cc57f340f83b0705c1d15092178a
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-04-19 00:44:20 +0300

    added login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'', loadComponent:()=>import('./homepage/homepage.component').then(h=>h.HomepageComponent)},
+  {path:'login', loadComponent:()=>import('../app//login/login.component').then(l=>l.LoginComponent)},
 ];
 
 @NgModule({
