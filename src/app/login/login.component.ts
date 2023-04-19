3e5896c561b4e2909c7962f2c37c3e6b48da5889
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-04-19 21:52:54 +0300

    updated code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule,ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  form!:FormGroup
+  constructor(private fb:FormBuilder) { }
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      Email:[null, [Validators.required, Validators.email]],
+      Password:[null, [Validators.required]],
 
+    }) 
+
+  }
 }
